4f81d06d158597cb444974d01a4eea0a86652da0
Aidan <aidan.hedges.470@accesscreative.ac.uk>
2024-12-27 14:49:30 +0000

    followed guide perfectly, not working, saying cannot import 'Enemy' from 'enemies

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
+from enemies import Enemy
 import constants as c
-
+import os
 pg.init()
 
 #create clock
@@ -10,6 +11,17 @@ clock = pg.time.Clock()
 #making screen
 screen = pg.display.set_mode((c.SCREEN_WIDTH,c.SCREEN_HEIGHT))
 pg.display.set_caption("Tower defence")
+
+#load images
+enemy_image = pg.image.load('C:\\Users\Aidan Hedges\\Documents\\coding\\games\\Ducky-tower-defence\\assets\\images\\enemies\\enemy_1.png').convert_alpha()
+
+#create group
+enemy_group = pg.sprite.Group()
+
+
+enemy = Enemy((200,300), enemy_image)
+enemy_group.add(enemy)
+
 
 #game loop
 run = True
