dc3fd4a5fbb649fa4cec1743924b69755a32affe
Aidan <aidan.hedges.470@accesscreative.ac.uk>
2024-12-27 13:30:49 +0000

    Made the window stay until closed

--- start.py
+++ start.py
@@ -6,3 +6,12 @@ screen_width = 800
 screen_height = 600
 
 screen = pygame.display.set_mode((screen_width,screen_height))
+
+run = True
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
